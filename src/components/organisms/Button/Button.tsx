940a1a8715feee6c4d93f096bb56f138b20b4a32
Igor Noskov <inoskov@suvie.com>
2021-02-02 23:07:03 +0300

    Use memo with components

--- src/components/organisms/Button/Button.tsx
+++ src/components/organisms/Button/Button.tsx
@@ -1,27 +1,25 @@
-import React, { FC, ButtonHTMLAttributes } from 'react'
+import React, { FC, ButtonHTMLAttributes, memo } from 'react'
 import classNames from 'classnames'
 
 export type TButtonProps = {
   isFullWidth?: boolean
 } & ButtonHTMLAttributes<HTMLButtonElement>
 
-export const Button: FC<TButtonProps> = ({
-  isFullWidth = true,
-  className = '',
-  ...props
-}: TButtonProps) => (
-  <button
-    className={classNames(
-      'inline-block font-medium text-center uppercase border rounded py-2 px-4',
-      'text-primary border-primary',
-      'hover:text-white hover:border-white hover:bg-primary',
-      'dark:text-white dark:border-white',
-      'dark:hover:text-primary dark:hover:border-primary dark:hover:bg-white',
-      className,
-      { 'w-full': isFullWidth }
-    )}
-    {...props}
-  />
+export const Button: FC<TButtonProps> = memo(
+  ({ isFullWidth = true, className = '', ...props }: TButtonProps) => (
+    <button
+      className={classNames(
+        'inline-block font-medium text-center uppercase border rounded py-2 px-4',
+        'text-primary border-primary',
+        'hover:text-white hover:border-white hover:bg-primary',
+        'dark:text-white dark:border-white',
+        'dark:hover:text-primary dark:hover:border-primary dark:hover:bg-white',
+        className,
+        { 'w-full': isFullWidth }
+      )}
+      {...props}
+    />
+  )
 )
 
 Button.displayName = 'Button'
