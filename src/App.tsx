c356610ea8db6ec54d49217b51a9af722655aa0f
Никита Бабеев <babeev.nikita@gmail.com>
2021-02-03 22:57:37 +0700

    task_4
    Fixed versions of Router

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ import './styles/globals.css'
 const App: React.FC = () => (
   <main>
     <Switch>
-      <Route exact path='/'/>
-      <Route path='/signup'/>
-      <Route path='/game'/>
-      <Route path='/profile'/>
-      <Route path='/liderboard'/>
-      <Route path='/400'/>
-      <Route path='/500'/>
-      <Route path='/forum'/>
+      <Route exact path="/" />
+      <Route path="/signup" />
+      <Route path="/game" />
+      <Route path="/profile" />
+      <Route path="/liderboard" />
+      <Route path="/400" />
+      <Route path="/500" />
+      <Route path="/forum" />
     </Switch>
   </main>
 )
