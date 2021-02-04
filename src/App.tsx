8537235ee63490644e10504e47f83e37eed4a681
Никита Бабеев <babeev.nikita@gmail.com>
2021-02-04 12:04:56 +0700

    Left Router only inside App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import './styles/globals.css'
 
 const App: React.FC = () => (
-  <main>
-    <Switch>
-      <Route exact path="/" />
-      <Route path="/signup" />
-      <Route path="/game" />
-      <Route path="/profile" />
-      <Route path="/liderboard" />
-      <Route path="/400" />
-      <Route path="/500" />
-      <Route path="/forum" />
-    </Switch>
-  </main>
+  <BrowserRouter>
+    <main>
+      <Switch>
+        <Route exact path="/" />
+        <Route path="/signup" />
+        <Route path="/game" />
+        <Route path="/profile" />
+        <Route path="/liderboard" />
+        <Route path="/400" />
+        <Route path="/500" />
+        <Route path="/forum" />
+      </Switch>
+    </main>
+  </BrowserRouter>
 )
 
 export default App
