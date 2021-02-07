b1062d82dfc664bbc14c6e5563737b3880ed1462
Igor Noskov <inoskov@suvie.com>
2021-02-07 22:42:46 +0300

    Update TumblerTheme component

--- src/components/organisms/TumblerTheme/TumblerTheme.tsx
+++ src/components/organisms/TumblerTheme/TumblerTheme.tsx
@@ -1,13 +1,45 @@
-import React, { FC, memo } from 'react'
-import { tumbler } from '@images'
+import React, { FC, memo, useCallback, useEffect, useState } from 'react'
+import { moon } from '@images'
 
 type TTumblerThemeProps = {}
 
-const TumblerTheme: FC<TTumblerThemeProps> = memo(() => (
-  <button className="my-auto mr-4">
-    <img src={tumbler} alt="" />
-  </button>
-))
+const TumblerTheme: FC<TTumblerThemeProps> = memo(() => {
+  const darkThemeClass = 'dark'
+
+  const [isDarkTheme, setIsDarkTheme] = useState(true)
+
+  const toggleTheme = useCallback(() => {
+    setIsDarkTheme((value) => !value)
+  }, [setIsDarkTheme])
+
+  useEffect(() => {
+    if (isDarkTheme) {
+      document.documentElement.classList.add(darkThemeClass)
+    } else {
+      document.documentElement.classList.remove(darkThemeClass)
+    }
+  }, [isDarkTheme])
+
+  return (
+    <button
+      className="relative bg-primary border-2 border-white rounded-2xl h-5 outline-none"
+      style={{ width: '3.125rem' }}
+      onClick={toggleTheme}
+    >
+      <img
+        width="25"
+        height="25"
+        src={moon}
+        className="absolute transition ease-in-out"
+        style={{
+          top: 'calc(50% - 12.5px)',
+          transform: isDarkTheme ? 'translateX(-2px)' : 'translateX(100%)'
+        }}
+        alt="Toggle theme"
+      />
+    </button>
+  )
+})
 
 TumblerTheme.displayName = 'TumblerTheme'
 
--- src/components/organisms/TumblerTheme/test.tsx
+++ src/components/organisms/TumblerTheme/test.tsx
@@ -1,12 +1,51 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import toJSON from 'enzyme-to-json'
 
 import { TumblerTheme } from '.'
 
 describe('<TumblerTheme />', () => {
+  const darkThemeClass = 'dark'
+
   it('should renders correct <TumblerTheme />', () => {
     const wrapper = shallow(<TumblerTheme />)
+
+    expect(toJSON(wrapper)).toMatchSnapshot()
+  })
+
+  it('should toggle theme', () => {
+    const wrapper = mount(<TumblerTheme />)
+
+    expect(wrapper.find('img').prop('style')).toEqual({
+      top: 'calc(50% - 12.5px)',
+      transform: 'translateX(-2px)'
+    })
+
+    expect(
+      document.documentElement.classList.contains(darkThemeClass)
+    ).toBeTruthy()
+
+    wrapper.simulate('click')
+
+    expect(wrapper.find('img').prop('style')).toEqual({
+      top: 'calc(50% - 12.5px)',
+      transform: 'translateX(100%)'
+    })
+
+    expect(
+      document.documentElement.classList.contains(darkThemeClass)
+    ).toBeFalsy()
+
+    wrapper.simulate('click')
+
+    expect(wrapper.find('img').prop('style')).toEqual({
+      top: 'calc(50% - 12.5px)',
+      transform: 'translateX(-2px)'
+    })
+
+    expect(
+      document.documentElement.classList.contains(darkThemeClass)
+    ).toBeTruthy()
 
     expect(toJSON(wrapper)).toMatchSnapshot()
   })
