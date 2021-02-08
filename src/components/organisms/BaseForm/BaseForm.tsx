5b39e74f7182394f00d8d9dfea2d9ed4cbb0547a
Никита Бабеев <babeev.nikita@gmail.com>
2021-02-08 22:35:51 +0700

    add desctructure

--- src/components/organisms/BaseForm/BaseForm.tsx
+++ src/components/organisms/BaseForm/BaseForm.tsx
@@ -17,30 +17,32 @@ type TBaseFormProps = {
   textButton: string
 }
 
-const BaseForm: FC<TBaseFormProps> = memo((props) => {
-  return (
-    <Formik
-      initialValues={props.initialValues}
-      validationSchema={props.validate}
-      onSubmit={(
-        values: IValues,
-        { setSubmitting }: FormikHelpers<IValues>
-      ) => {
-        console.log(JSON.stringify(values, null, 2))
-        setSubmitting(false)
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form noValidate>
-          {props.children}
-          <Button className="mb-4" type="submit" disabled={isSubmitting}>
-            {props.textButton}
-          </Button>
-        </Form>
-      )}
-    </Formik>
-  )
-})
+const BaseForm: FC<TBaseFormProps> = memo(
+  ({ initialValues, validate, children, textButton }) => {
+    return (
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validate}
+        onSubmit={(
+          values: IValues,
+          { setSubmitting }: FormikHelpers<IValues>
+        ) => {
+          console.log(JSON.stringify(values, null, 2))
+          setSubmitting(false)
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form noValidate>
+            {children}
+            <Button className="mb-4" type="submit" disabled={isSubmitting}>
+              {textButton}
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    )
+  }
+)
 BaseForm.displayName = 'BaseForm'
 
 export default BaseForm
