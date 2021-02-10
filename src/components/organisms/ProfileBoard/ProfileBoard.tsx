59181782e2225d8097c6e34ff270d1519c0ddc6c
Igor Noskov <inoskov@suvie.com>
2021-02-10 12:39:19 +0300

    Add actions list to ProfileBoard

--- src/components/organisms/ProfileBoard/ProfileBoard.tsx
+++ src/components/organisms/ProfileBoard/ProfileBoard.tsx
@@ -1,22 +1,30 @@
 import React, { FC, memo } from 'react'
 import classNames from 'classnames'
+import { ActionsListItem, NameValueListItem } from '@components/atoms'
 import { List } from '@components/molecules'
-import { NameValueListItem } from '@components/atoms'
 
 type TProfileBoardProps = {}
 
-const ProfileBoard: FC<TProfileBoardProps> = memo(() => (
-  <div className={classNames('text-primary', 'dark:text-white')}>
-    <List className="mb-12">
-      <NameValueListItem name="Email" value="ivan@yandex.ru" />
-      <NameValueListItem name="Login" value="IvanIvanov" />
-      <NameValueListItem name="First name" value="Ivan" />
-      <NameValueListItem name="Last name" value="Ivanov" />
-      <NameValueListItem name="Phone number" value="+790000000000" />
-      <NameValueListItem name="Password" value="••••••" />
-    </List>
-  </div>
-))
+const ProfileBoard: FC<TProfileBoardProps> = memo(() => {
+  const handleLogoutButtonClick = () => {}
+
+  return (
+    <div className={classNames('text-primary', 'dark:text-white')}>
+      <List className="mb-12">
+        <NameValueListItem name="Email" value="ivan@yandex.ru" />
+        <NameValueListItem name="Login" value="IvanIvanov" />
+        <NameValueListItem name="First name" value="Ivan" />
+        <NameValueListItem name="Last name" value="Ivanov" />
+        <NameValueListItem name="Phone number" value="+790000000000" />
+        <NameValueListItem name="Password" value="••••••" />
+      </List>
+
+      <List>
+        <ActionsListItem name="Log out" onClick={handleLogoutButtonClick} />
+      </List>
+    </div>
+  )
+})
 
 ProfileBoard.displayName = 'ProfileBoard'
 
