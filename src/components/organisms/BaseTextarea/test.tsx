96b3cc7f7a66fedb949e55933987534c709c1f7c
Igor Noskov <inoskov@suvie.com>
2021-02-11 13:35:03 +0300

    Add TopicMessageForm and BaseTextarea

---
+++ src/components/organisms/BaseTextarea/test.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJSON from 'enzyme-to-json'
+import { Formik } from 'formik'
+
+import { BaseTextarea } from '.'
+
+describe('<BaseTextarea />', () => {
+  const initialValues = { test: '' }
+  const onSubmit = jest.fn()
+  const name = 'test_name'
+  const placeholder = 'Test placeholder'
+
+  it('should renders correct <BaseTextarea />', () => {
+    const wrapper = shallow(
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        <BaseTextarea name={name} placeholder={placeholder} />
+      </Formik>
+    )
+
+    expect(toJSON(wrapper)).toMatchSnapshot()
+  })
+})
