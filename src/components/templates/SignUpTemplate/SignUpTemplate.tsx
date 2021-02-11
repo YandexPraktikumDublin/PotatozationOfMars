fa2d3a0b891b29c247daab84920312bd46e00c42
Igor Noskov <inoskov@suvie.com>
2021-02-11 14:03:01 +0300

    Fix templates

--- src/components/templates/SignUpTemplate/SignUpTemplate.tsx
+++ src/components/templates/SignUpTemplate/SignUpTemplate.tsx
@@ -3,23 +3,19 @@ import { FormLink } from '@components/atoms'
 import { PATHS } from '@config'
 import { Window, Title, SignUpForm } from '@components/organisms'
 
-type TSignUpTemplateProps = {
-  title: string
-}
+type TSignUpTemplateProps = {}
 
-const SignUpTemplate: FC<TSignUpTemplateProps> = memo(
-  ({ title }: TSignUpTemplateProps) => (
-    <div className="w-full max-w-xs">
-      <Window>
-        <Title>{title}</Title>
-        <SignUpForm />
-        <FormLink path={PATHS.AUTH} className="inline-block mt-4">
-          Already have an account?
-        </FormLink>
-      </Window>
-    </div>
-  )
-)
+const SignUpTemplate: FC<TSignUpTemplateProps> = memo(() => (
+  <div className="w-full max-w-xs">
+    <Window>
+      <Title>Sign up</Title>
+      <SignUpForm />
+      <FormLink path={PATHS.AUTH} className="inline-block mt-4">
+        Already have an account?
+      </FormLink>
+    </Window>
+  </div>
+))
 
 SignUpTemplate.displayName = 'SignUpTemplate'
 
