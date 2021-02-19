461ca86a92990b1c464f23dc9c32958e0a83b006
Ismail Ajiev <iadjiev@gmail.com>
2021-02-19 21:35:25 +0300

    Added Fullscreen API and minor fixes

--- src/components/organisms/GameWindow/GameWindow.tsx
+++ src/components/organisms/GameWindow/GameWindow.tsx
@@ -1,10 +1,41 @@
-import React, { FC, memo } from 'react'
-import { GameCanvas } from '@components/molecules'
+import React, {FC, memo, useEffect, useRef, useState} from 'react'
+import { GameCanvas, NavigationButton } from '@components/molecules'
+import {fullscreenOn, fullscreenOff} from '@images'
+import { InputsController } from '@game/controllers'
 
 type TGameWindowProps = {}
 
 const GameWindow: FC<TGameWindowProps> = memo(() => {
-  return <GameCanvas />
+  const windowRef = useRef<HTMLDivElement>(null)
+  const [FSIcon, setFSIcon] = useState(fullscreenOn);
+
+  const toggleFullScreen = (toggle = true) => {
+    if (!toggle) return
+    const gameWindow = windowRef.current as HTMLDivElement
+    if (!document.fullscreenElement && document.fullscreenEnabled) {
+      gameWindow.requestFullscreen().then(()=>{setFSIcon(fullscreenOff)}).catch()
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen().then(()=>{setFSIcon(fullscreenOn)}).catch()
+      }
+    }
+  }
+
+  useEffect(() => {
+    return InputsController.onKeyPress(toggleFullScreen, ['KeyF'])
+  }, [windowRef.current])
+
+  return (
+    <div className="relative flex justify-center items-center" ref={windowRef}>
+      <GameCanvas />
+      <NavigationButton
+        className="absolute top-3 right-3"
+        title="Full screen"
+        onClick={toggleFullScreen}
+        imageSrc={FSIcon}
+      />
+    </div>
+  )
 })
 
 GameWindow.displayName = 'GameWindow'
--- src/components/templates/GameTemplate/GameTemplate.tsx
+++ src/components/templates/GameTemplate/GameTemplate.tsx
@@ -4,11 +4,7 @@ import { GameWindow } from '@components/organisms'
 type TGameTemplateProps = {}
 
 const GameTemplate: FC<TGameTemplateProps> = memo(() => {
-  return (
-    <div className="flex justify-center flex-grow">
-      <GameWindow />
-    </div>
-  )
+  return <GameWindow />
 })
 
 GameTemplate.displayName = 'GameTemplate'
