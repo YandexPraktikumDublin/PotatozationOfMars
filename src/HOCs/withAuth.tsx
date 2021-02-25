0edd48aaaa295ae65787bf0fde64b52d18c2b282
Igor Noskov <inoskov@suvie.com>
2021-02-26 01:42:44 +0300

    Fix withAuth and withAuthCheck HOCs

--- src/HOCs/withAuth.tsx
+++ src/HOCs/withAuth.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchUserRequest } from '@store/user/fetchUser/actions'
 import {
@@ -10,18 +10,25 @@ import { PATHS } from '@config'
 
 export default function withAuth<T>(Component: React.FC<T>) {
   return (props: any) => {
+    const [isInitialized, setIsInitialized] = useState<boolean>(false)
+
     const dispatch = useDispatch()
     const user = useSelector(getUserSelector)
     const pendingUser = useSelector(getUserPendingSelector)
 
     useEffect(() => {
+      setIsInitialized(true)
       dispatch(fetchUserRequest())
     }, [])
 
-    if (!user && pendingUser) {
-      return null
+    if (!isInitialized || (!user && pendingUser)) {
+      return null // TODO: add loader
     }
 
-    return user ? <Component {...props} /> : <Redirect to={PATHS.AUTH} />
+    if (isInitialized && !user && !pendingUser) {
+      return <Redirect to={PATHS.AUTH} />
+    }
+
+    return <Component {...props} />
   }
 }
