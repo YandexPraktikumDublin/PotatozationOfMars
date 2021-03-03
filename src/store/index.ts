b781273777c8ff72d73f72f71f444c1a0221e305
Igor Noskov <inoskov@suvie.com>
2021-03-03 20:18:06 +0300

    Fix rootSaga export

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import logger from 'redux-logger'
 import rootReducer from '@store/rootReducer'
-import { rootSaga } from '@store/rootSaga'
+import rootSaga from '@store/rootSaga'
 
 const sagaMiddleware = createSagaMiddleware()
 
--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -7,7 +7,7 @@ import signupSaga from '@store/signup/sagas'
 import authSaga from '@store/auth/sagas'
 import logoutSaga from '@store/logout/sagas'
 
-export function* rootSaga() {
+export default function* rootSaga() {
   yield all([
     fork(userSaga),
     fork(updateUserSaga),
