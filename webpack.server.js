89f38a0535b3115048e839eaa11df8d7ed058c60
Igor Noskov <inoskov@suvie.com>
2021-03-10 18:20:30 +0300

    Remove null-loader

--- webpack.server.js
+++ webpack.server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const nodeExternals = require('webpack-node-externals')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   name: 'server',
@@ -24,7 +25,8 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: 'null-loader'
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
@@ -56,5 +58,8 @@ module.exports = {
   },
   externals: [nodeExternals({ allowlist: [/\.(?!(?:tsx?|json)$).{1,5}$/i] })],
   optimization: { nodeEnv: false },
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: '[name].css' })
+  ]
 }
