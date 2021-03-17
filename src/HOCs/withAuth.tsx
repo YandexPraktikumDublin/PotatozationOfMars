b5c4978fd9d767939c2ba9e5540d0154ec9e8e69
Igor Noskov <inoskov@suvie.com>
2021-03-17 16:58:07 +0400

    Fix auth redirect

--- src/HOCs/withAuth.tsx
+++ src/HOCs/withAuth.tsx
@@ -1,16 +1,41 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchUserRequest } from '@store/user/fetchUser/actions'
-import { getUserSelector } from '@store/user/fetchUser/selectors'
+import {
+  getUserErrorSelector,
+  getUserPendingSelector,
+  getUserSelector
+} from '@store/user/fetchUser/selectors'
+import { Redirect, useLocation } from 'react-router'
+import { PATHS } from '@config'
 
 export default function withAuth<T>(Component: React.FC<T>) {
   return (props: any) => {
+    const location = useLocation()
+
     const dispatch = useDispatch()
     const user = useSelector(getUserSelector)
+    const pendingUser = useSelector(getUserPendingSelector)
+    const errorUser = useSelector(getUserErrorSelector)
+
+    const isAuthOrSignup =
+      location.pathname === PATHS.AUTH || location.pathname === PATHS.SIGNUP
 
     useEffect(() => {
-      if (!user) dispatch(fetchUserRequest())
+      if (!user && !isAuthOrSignup) dispatch(fetchUserRequest())
     }, [])
+
+    if (pendingUser) {
+      return null
+    }
+
+    if (user && isAuthOrSignup) {
+      return <Redirect to={PATHS.BASE} />
+    }
+
+    if (errorUser && !isAuthOrSignup) {
+      return <Redirect to={PATHS.AUTH} />
+    }
 
     return <Component {...props} />
   }
