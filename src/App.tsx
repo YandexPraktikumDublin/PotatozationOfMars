7bce4ee014c4ef011aaf5ea483f1f9a1994a57e1
Igor Noskov <inoskov@suvie.com>
2021-03-23 20:48:08 +0400

    Progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import { hot } from 'react-hot-loader/root'
+import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import ErrorBoundary from './ErrorBoundary'
 import { background } from '@images'
