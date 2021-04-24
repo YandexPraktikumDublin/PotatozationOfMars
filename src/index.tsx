97518b2e73a5f2d8409d7805084affa046a74a9e
Никита Бабеев <babeev.nikita@gmail.com>
2021-04-24 18:40:00 +0700

    add service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,22 @@ import { ConnectedRouter } from 'connected-react-router'
 import { store, history } from '@store/store'
 import App from './App'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((registration) => {
+        console.info(
+          'ServiceWorker registration successful with scope: ',
+          registration.scope
+        )
+      })
+      .catch((error: string) => {
+        console.error('ServiceWorker registration failed: ', error)
+      })
+  })
+}
+
 const RootComponent = () => (
   <ReduxProvider store={store}>
     <ConnectedRouter history={history}>
